30fb7a9fed0e83f5152bdddac1ca6a34ce56d58e
Object-ions <moshikoatia@gmail.com>
2024-05-05 15:29:58 -0700

    Add error and exception handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,26 @@
-const registerUser = (req, res) => {
+const asyncHandler = require('express-async-handler');
+
+// Register a new a new user
+// /api/users
+// public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please include all fields');
+  }
+
   res.send('Register Route');
-};
-const loginUser = (req, res) => {
+});
+
+// User a login
+// /api/users/login
+// public
+const loginUser = asyncHandler(async (req, res) => {
   res.send('Login Route');
-};
+});
 
 module.exports = {
   registerUser,
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT || 8000;
+
 const app = express();
+
+// In order to get data from the body: - 2 steps
+// 1. This will allow us to send raw JSON
+app.use(express.json());
+
+// 2. To accept the URL encoded form
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.send('Hello');
@@ -9,5 +18,7 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
