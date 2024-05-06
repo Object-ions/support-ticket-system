cd04bf8078b56763adff05314f7ac2e88312fe89
Object-ions <moshikoatia@gmail.com>
2024-05-06 10:07:23 -0700

    Add Home structure

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import { FaQuestionCircle, FaTabletAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="heading">
+        <h1>What do you need help with?</h1>
+        <p>Please choose from an option below</p>
+      </section>
+
+      <Link to="/new-ticket" className="btn btn-reverse btn-block">
+        <FaQuestionCircle /> Create New Ticket
+      </Link>
+      <Link to="/tickets" className="btn btn-block">
+        <FaTabletAlt /> view My Ticket
+      </Link>
+    </>
+  );
 };
 
 export default Home;
