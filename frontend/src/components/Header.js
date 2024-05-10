247272ae4528d4d6a2d077370b44f4fe72f50160
Object-ions <moshikoatia@gmail.com>
2024-05-09 18:19:45 -0700

    Clean typo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,7 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
-import { LogOutput, logout, reset } from '../features/auth/authSlice';
+import { logout, reset } from '../features/auth/authSlice';
 
 const Header = () => {
   const navigate = useNavigate();
