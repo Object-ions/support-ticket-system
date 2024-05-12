edfade5edd2f94d053b8ce60a16d37350953daca
Object-ions <moshikoatia@gmail.com>
2024-05-12 14:29:37 -0700

    Successfully get and create a ticket

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -7,14 +7,46 @@ const Ticket = require('../models/ticketModel');
 // GET - /api/tickets
 // private
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get tickets' });
+  // Get user using th id and JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+
+  res.status(200).json(tickets);
 });
 
 // Create new ticket
 // POST - /api/tickets/new
 // private
 const createTicket = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Create ticket' });
+  const { product, description } = req.body;
+
+  if (!product || !description) {
+    res.status(400);
+    throw new Error('Please add a  product and description');
+  }
+
+  // Get user using th id and JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.create({
+    product,
+    description,
+    user: req.user.id,
+    status: 'new',
+  });
+
+  res.status(201).json(ticket);
 });
 
 module.exports = {
