5b0c859c1380dfecc845948e6bed1b68a460c7ae
Object-ions <moshikoatia@gmail.com>
2024-05-21 18:27:04 -0700

    Successfully create ticket fx

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -10,6 +10,26 @@ const initialState = {
   message: '',
 };
 
+// Create new ticket
+export const createTicket = createAsyncThunk(
+  'tickets/create',
+  async (ticketData, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await ticketService.createTicket(ticketData, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString;
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 export const ticketSlice = createSlice({
   name: 'ticket',
   initialState,
@@ -17,7 +37,20 @@ export const ticketSlice = createSlice({
     reset: (state) => initialState,
   },
 
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(createTicket.pending, (state) => {
+      state.isLoading = true;
+    });
+    builder.addCase(createTicket.fulfilled, (state) => {
+      state.isLoading = false;
+      state.isSuccess = true;
+    });
+    builder.addCase(createTicket.rejected, (state, action) => {
+      state.isLoading = false;
+      state.isError = true;
+      state.message = action.payload;
+    });
+  },
 });
 
 export const { reset } = ticketSlice.actions;
