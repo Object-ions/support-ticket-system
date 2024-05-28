f22307686e22e0a83b0e66f8cfa020c99dae85d8
Object-ions <moshikoatia@gmail.com>
2024-05-27 21:53:11 -0700

    Add close ticket fx

--- frontend/src/pages/Ticket.js
+++ frontend/src/pages/Ticket.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { getTicket, reset } from '../features/tickets/ticketSlice';
+import { getTicket, reset, closeTicket } from '../features/tickets/ticketSlice';
 import BackButton from '../components/BackButton';
 import Spinner from '../components/Spinner';
 import { toast } from 'react-toastify';
@@ -12,6 +12,7 @@ const Ticket = () => {
   );
 
   const params = useParams();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const { ticketId } = useParams();
 
@@ -23,6 +24,12 @@ const Ticket = () => {
     dispatch(getTicket(ticketId));
     //eslint-disable-next-line
   }, [isError, message, ticketId]);
+
+  const onTicketClose = () => {
+    dispatch(closeTicket(ticketId));
+    toast.success('ticket closed');
+    navigate('/tickets');
+  };
 
   if (isLoading) {
     return <Spinner />;
@@ -50,7 +57,14 @@ const Ticket = () => {
             <p>{ticket.description}</p>
           </div>
         </h3>
+        <h3>Product: {ticket.product}</h3>
       </header>
+
+      {ticket.status !== 'closed' && (
+        <button className="btn btn-block btn-danger" onClick={onTicketClose}>
+          Close Ticket
+        </button>
+      )}
     </div>
   );
 };
